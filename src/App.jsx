0883f5c74de124696a9a90ab8d4674e83759450e
Telenchak Oleksiy <letel315@gmail.com>
2023-12-27 15:51:10 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="homepage">
+        <nav className="navbar">
+          <a href="">About</a>
+          <a href="">Projects</a>
+          <a href=""></a>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default App;
