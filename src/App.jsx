9dfb08ed14127bdbe6a45d3fd0999e111f50d3d7
Telenchak Oleksiy <letel315@gmail.com>
2023-12-28 01:22:13 +0200

    add animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ function App() {
     <>
       <div className="homepage">
         <div className="fullscreen">
-          <div className="homepage__container">
-            <p>Oleksii Telecnhak</p>
+          <div className="fullscreen__contant-container">
+            {/* <p>Oleksii Telecnhak</p>
             <h1>Front End Developer</h1>
-            <p>Click for more</p>
+            <p>Click for more</p> */}
+            <div className="animation-container">
+              <div className="circle green"></div>
+              <div className="circle blue"></div>
+              <div className="circle red"></div>
+            </div>
           </div>
         </div>
       </div>
