c28ab5bbb568f604ed949171c3ca5383099f6015
Telenchak Oleksiy <letel315@gmail.com>
2023-12-29 00:20:38 +0200

    finnaly added fullheader

--- src/components/FullScreenHeader.jsx
+++ src/components/FullScreenHeader.jsx
@@ -1,30 +1,38 @@
-import Video from "../public/blackhole.webm";
+import BlackHoleAnimation from "./BlackHoleAnimation";
+import StarsCanvas from "./StarBackground";
 import { FaArrowDown } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { slideInFromLeft, slideInFromTop } from "../utils/motion";
 export default function FullScreenHeader() {
   return (
-    <div className="container bg-black min-w-full">
-      <div className=" pl-24 w-[550px]  min-h-screen text-white  ">
-        <div className="flex flex-col min-h-[100vh] justify-center text-2xl z-10  relative">
-          <p className="mt-14">Oleksii Telecnhak</p>
-          <h1 className="font-medium text-5xl my-10">Front End Developer</h1>
-          <div className="w-[300px]">
-            <p>Click for more</p>
-            <div className="mx-auto mt-5 inline-block">
-              <button className="cursor-pointer  flex items-center justify-center w-12 h-12 border-2 border-white rounded-full transition-all hover:scale-110 hover:opacity-80">
-                <FaArrowDown />
-              </button>
+    <>
+      <div className="container bg-black  min-w-full">
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          className=" pl-24 w-[550px]   min-h-screen text-white  "
+        >
+          <motion.div
+            variants={slideInFromTop}
+            className="flex flex-col min-h-[100vh] justify-center text-2xl z-10  relative"
+          >
+            <p className="mt-14">Oleksii Telecnhak</p>
+            <h1 className="font-medium text-5xl my-10">Front End Developer</h1>
+            <div>
+              <p>Click for more</p>
+              <div className="mx-auto mt-5 inline-block">
+                <button className="cursor-pointer mx-12  flex items-center justify-center w-12 h-12 border-2 border-white rounded-full transition-all hover:scale-110 hover:opacity-80">
+                  <FaArrowDown />
+                </button>
+              </div>
             </div>
-          </div>
-        </div>
+          </motion.div>
 
-        <video
-          src={Video}
-          autoPlay
-          muted
-          loop
-          className="w-full z-[0] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[1] h-[100vh] object-cover mix-blend-screen"
-        ></video>
+          <StarsCanvas className=" z-[-100]" />
+          <BlackHoleAnimation />
+        </motion.div>
       </div>
-    </div>
+      <div className="h-[100vh]"></div>
+    </>
   );
 }
