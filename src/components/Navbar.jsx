a53053872514071fa5a5084f2fb7a3fb69283cc6
Telenchak Oleksiy <letel315@gmail.com>
2024-01-13 23:34:56 +0200

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,36 +2,42 @@ import { useState } from "react";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  function closeMenu() {
+    setIsOpen(false);
+  }
   return (
     <>
-      <nav className="w-full  h-[65px] fixed top-0 shadow-lg  backdrop-blur-md z-[110]  px-10">
+      <nav className="w-full  h-[65px] fixed top-0 shadow-lg  backdrop-blur-md z-[110]   px-10">
         <div
           style={{ display: isOpen ? "flex" : "none" }}
           className="fixed text-xl sm:text-md top-0 bg-black bg-opacity-95 right-0 h-screen w-full backdrop-blur-[10px] flex flex-col items-center  justify-start  text-gray-200"
         >
-          <div className="flex mb-[15px] bg-[#3b1c87]  px-[10px] justify-between w-full items-center">
-            <p className="text-xl">Oleksii Telenchak</p>
+          <div className="flex my-[15px]   px-[10px] justify-end w-full items-center">
             <IoMdClose
               size={35}
-              onClick={() => setIsOpen((prev) => !prev)}
+              onClick={closeMenu}
               className="cursor-pointer"
             />
           </div>
 
           <a
             href="#about-me"
+            onClick={closeMenu}
             className="cursor-pointer py-[15px] px-[10px] hover:scale-110  duration-150"
           >
             About me
           </a>
           <a
             href="#skills"
+            onClick={closeMenu}
             className="cursor-pointer py-[15px] px-[10px] hover:scale-110  duration-150"
           >
             Skills
           </a>
           <a
             href="#projects"
+            onClick={closeMenu}
             className="cursor-pointer py-[15px] px-[10px] hover:scale-110 duration-150"
           >
             Projects
@@ -69,7 +75,7 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-        <div className=" hidden md:flex relative top-[15px] justify-end">
+        <div className=" hidden md:flex relative top-[15px] left-[20px] justify-end">
           {!isOpen && (
             <IoMdMenu
               className="cursor-pointer "
