6ecba24215f4d3d0c342787daf16e806b6beadb5
Telenchak Oleksiy <letel315@gmail.com>
2024-02-08 13:11:09 +0200

    Add projects comp

--- src/components/sub/Projects.jsx
+++ src/components/sub/Projects.jsx
@@ -1,24 +1,94 @@
-import { useRef } from "react";
-import { motion, useSpring, useScroll } from "framer-motion";
-import ProjectCard from "../ProjectCard";
-import { slideInFromTop } from "../../utils/motion.js";
+import { motion } from "framer-motion";
+
+import { staggerContainer } from "../../utils/motion.js";
+import github from "../../public/github.png";
 import { projects } from "../../constants/projects.js";
+import { fadeIn, textVariant } from "../../utils/motion";
 
-export default function Projects() {
+const ProjectCard = ({ index, title, description, tags, img, githubLink }) => {
   return (
-    <section className="relative no-scroll">
-      <div className=" relative top-[270px] top-0 left-0 pt-[50px] text-center text-4xl md:text-2xl">
-        <h2
-          id="projects"
-          className="text-center -translate-y-[50px] mb-[100px] pt-[70px] text-5xl"
-        >
-          Projects
-        </h2>
-      </div>
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <div className="bg-tertiary hover:scale-105 transition-all p-5 rounded-2xl sm:w-[360px] w-full">
+        <div className="relative w-full h-[230px]">
+          <img
+            src={img}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl"
+          />
 
-      {projects.map((project) => (
-        
-      ))}
-    </section>
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(githubLink, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{title}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${title}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </div>
+    </motion.div>
   );
-}
+};
+
+const Works = () => {
+  return (
+    <>
+      <motion.section
+        variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className={`sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto relative z-0`}
+      >
+        <motion.div variants={textVariant()}>
+          <h2
+            className={`text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]`}
+          >
+            Projects.
+          </h2>
+        </motion.div>
+
+        <div className="w-full flex">
+          <motion.p
+            variants={fadeIn("", "", 0.1, 1)}
+            className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          >
+            Following projects showcases my skills and experience through
+            real-world examples of my work. Each project is briefly described
+            with links to code repositories and live demos in it. It reflects my
+            ability to solve complex problems, work with different technologies,
+            and manage projects effectively.
+          </motion.p>
+        </div>
+
+        <div className="mt-20 flex flex-wrap gap-7">
+          {projects.map((project, index) => (
+            <ProjectCard key={`project-${index}`} index={index} {...project} />
+          ))}
+        </div>
+      </motion.section>
+    </>
+  );
+};
+
+export default Works;
--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,26 +1,33 @@
+import currencyTracker from "../public/currency.png";
+import monsterSlayerGame from "../public/monsterSlayerGame.png";
+
 export const projects = [
   {
     title: "Crypto Currency Tracker",
     description:
       "This site allows you to add cryptocurrency and monitor its price.",
-    img: "src/public/currency.png",
+    img: currencyTracker,
     demoLink: "https://eloleksiy.github.io/currency-tracker/",
     githubLink: "https://github.com/ElOleksiy/currency-tracker, Tailwind",
-    stack: "React, Tailwind",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+    ],
   },
   {
     title: "Monsterr Slayer Game",
     description:
       "Turn-based game, the main goal of which is to fight a monster.",
-    img: "@/public/monsterSlayerGame.png",
+    img: monsterSlayerGame,
     demoLink: "https://github.com/ElOleksiy/vue-game",
     githubLink: "",
-    stack: "Vue JS",
+    tags: [
+      {
+        name: "vue",
+        color: "green-text-gradient",
+      },
+    ],
   },
 ];
-
-Array.prototype.last = function () {
-  console.log(this[this.length - 1]);
-};
-const num = [1, 2, 3];
-console.log(num.last());
