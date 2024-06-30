7bb988c7c97a2aa56a4be48edb8aeea9a865667e
Telenchak Oleksii <121349331+ElOleksii@users.noreply.github.com>
2024-06-30 13:46:37 +0300

    Update projects.js

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -8,7 +8,7 @@ export const projects = [
       "This site allows you to add cryptocurrency and monitor its price.",
     img: currencyTracker,
     demoLink: "https://eloleksiy.github.io/currency-tracker/",
-    githubLink: "https://github.com/ElOleksiy/currency-tracker",
+    githubLink: "https://github.com/ElOleksii/currency-tracker",
     tags: [
       {
         name: "react",
@@ -21,7 +21,7 @@ export const projects = [
     description:
       "Turn-based game, the main goal of which is to fight a monster.",
     img: monsterSlayerGame,
-    demoLink: "https://github.com/ElOleksiy/vue-game",
+    demoLink: "https://github.com/ElOleksii/vue-game",
     githubLink: "",
     tags: [
       {
