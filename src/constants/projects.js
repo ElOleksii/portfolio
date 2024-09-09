8b6c6d6309c1f2d9d5ad7fb16b63297b9d5c4e0d
Telenchak Oleksiy <letel315@gmail.com>
2024-09-09 16:37:48 +0300

    Add fitness landing to projects

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,5 +1,6 @@
 import currencyTracker from "../public/currency.png";
 import monsterSlayerGame from "../public/monsterSlayerGame.png";
+import fitnessWorkout from "../public/fitness-workout.png";
 
 export const projects = [
   {
@@ -21,12 +22,25 @@ export const projects = [
     description:
       "Turn-based game, the main goal of which is to fight a monster.",
     img: monsterSlayerGame,
-    demoLink: "",
+    demoLink: "https://eloleksii.github.io/vue-game",
     githubLink: "https://github.com/ElOleksii/vue-game",
     tags: [
       {
         name: "vue",
         color: "text-green-500",
+      },
+    ],
+  },
+  {
+    title: "Fitness Workout Landing",
+    description: "This is simple landing example about Fitness Workout",
+    img: fitnessWorkout,
+    demoLink: "https://eloleksii.github.io/fitness-site/",
+    githubLink: "https://github.com/ElOleksii/fitness-site",
+    tags: [
+      {
+        name: "scss",
+        color: "text-pink-500",
       },
     ],
   },
